980f20cb54c721e20ffea3f019da91fdca5a9e7e
Brij Bhatia <brijbhatia997@gmail.com>
2024-10-30 22:03:24 -0700

    Added UI Styling and Layers

---
+++ lib/zustand-context.tsx
@@ -0,0 +1,20 @@
+import { StoreApi } from "zustand";
+import React, { useContext } from "react";
+
+export const createZustandContext = <TInitial, TStore extends StoreApi<any>>(
+  getStore: (inital: TInitial) => TStore
+) => {
+  const Context = React.createContext(null as any as TStore);
+  const Provider = (props: {
+    children?: React.ReactNode;
+    initialValue: TInitial;
+  }) => {
+    const [store] = React.useState(getStore(props.initialValue));
+    return <Context.Provider value={store}>{props.children}</Context.Provider>;
+  };
+  return {
+    useContext: () => React.useContext(Context),
+    Context,
+    Provider,
+  };
+};
